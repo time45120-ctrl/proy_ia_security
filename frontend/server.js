912aa58c81eeb45d19a1070002f40793cd5ad52d
Abraham Caro <time45120@gmail.com>
2026-05-12 15:46:15 -0500

    f1.14

---
+++ frontend/server.js
@@ -0,0 +1,15 @@
+const { createServer } = require("node:http");
+const next = require("next");
+
+const hostname = "0.0.0.0";
+const port = Number(process.env.PORT || 3000);
+const app = next({ dev: false, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((request, response) => {
+    handle(request, response);
+  }).listen(port, hostname, () => {
+    console.log(`Frontend listo en http://${hostname}:${port}`);
+  });
+});
