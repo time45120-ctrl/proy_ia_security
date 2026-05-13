6aad15d4af4f690eb767331d5d105c28a29562cf
Abraham Caro <time45120@gmail.com>
2026-05-12 19:33:33 -0500

    f1.22

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,8 @@
 import { WelcomeGate } from "@/components/welcome-gate";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function Home() {
   return <WelcomeGate />;
 }
